f03923580e9933d10fa758b83085616a00e09634
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-09-19 20:46:25 +0200

    Better way to grab the packagemanager

--- appavailability.android.js
+++ appavailability.android.js
@@ -1,11 +1,10 @@
 var application = require("application");
-var context = application.android.context;
 
 exports.available = function (uri) {
   return new Promise(function (resolve, reject) {
     try {
-      var pm = context.getPackageManager();
       try {
+        var pm = com.tns.NativeScriptApplication.getInstance().getPackageManager();
         pm.getPackageInfo(uri, android.content.pm.PackageManager.GET_ACTIVITIES);
         resolve(true);
       } catch(e) {
