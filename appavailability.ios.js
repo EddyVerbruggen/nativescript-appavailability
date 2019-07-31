e4fa26b09585db81210c5017104ddbb16fb0566e
Shiva Prasad <sp@shiv19.com>
2019-07-31 15:22:52 +1200

    utils.ios.getter is deprecated

--- appavailability.ios.js
+++ appavailability.ios.js
@@ -4,7 +4,7 @@ exports.available = function (scheme) {
   return new Promise(function (resolve, reject) {
     try {
       var url = NSURL.URLWithString(scheme);
-      var app = utils.ios.getter(UIApplication, UIApplication.sharedApplication);
+      var app = UIApplication.sharedApplication;
       var avail = app.canOpenURL(url);
       resolve(avail);
     } catch (ex) {
@@ -17,7 +17,7 @@ exports.available = function (scheme) {
 exports.availableSync = function (scheme) {
   try {
     var url = NSURL.URLWithString(scheme);
-    var app = utils.ios.getter(UIApplication, UIApplication.sharedApplication);
+    var app = UIApplication.sharedApplication;
     return app.canOpenURL(url);
   } catch (ex) {
     console.log("Error in appavailability.availableSync: " + ex);
